6a0421c67e5ba63e28c397cbe94f006c0610cfd2
ritamsonowal <ritamsonowal4@gmail.com>
2025-05-14 18:16:36 +0530

    ..

---
+++ Website/src/lib/config.ts
@@ -0,0 +1,9 @@
+export const MQTT_CONFIG = {
+  MQTT_BROKER_1: "wss://test.mosquitto.org:8081/mqtt",
+  MQTT_BROKER_2: "wss://broker.emqx.io:8084/mqtt",
+  MQTT_CLIENT_ID: "ESP32Client-Ritam1234",
+  MQTT_TOPICS: {
+    RELAY_1: "x12kf9_a1/relay/1",
+    RELAY_2: "x12kf9_a1/relay/2"
+  }
+};
